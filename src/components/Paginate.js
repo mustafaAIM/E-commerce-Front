74691fd6ff1a24fc469995c034863033a22f82d5
mustafaAIM <moustafa.alhaiba.2003@gmail.com>
2024-09-17 10:20:01 +0300

    fix

--- src/components/Paginate.js
+++ src/components/Paginate.js
@@ -3,23 +3,26 @@ import { Pagination } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 function Paginate({ pages, page, keyword = '', isAdmin = false }) {
-    const queryParams = new URLSearchParams();
-    if (keyword) queryParams.set('keyword', keyword);
-    
-    return (pages > 1 && (
-        <Pagination>
-            {[...Array(pages).keys()].map((x) => (
-                <LinkContainer
-                    key={x + 1}
-                    to={`${isAdmin ? '/admin/productlist' : '/'}?${queryParams.toString()}&page=${x + 1}`}
-                >
-                    <Pagination.Item active={x + 1 === page}>
-                        {x + 1}
-                    </Pagination.Item>
-                </LinkContainer>
-            ))}
-        </Pagination>
-    ));
+  const queryParams = new URLSearchParams();
+  if (keyword) queryParams.set('keyword', keyword);
+  
+  return (pages > 1 && (
+      <Pagination>
+          {[...Array(pages).keys()].map((x) => (
+              <LinkContainer
+                  key={x + 1}
+                  to={{
+                      pathname: isAdmin ? '/admin/productlist' : '/',
+                      search: `?${queryParams.toString()}&page=${x + 1}`
+                  }}
+              >
+                  <Pagination.Item active={x + 1 === page}>
+                      {x + 1}
+                  </Pagination.Item>
+              </LinkContainer>
+          ))}
+      </Pagination>
+  ));
 }
 
 export default Paginate;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import store from './store'
-//import './index.css';
-import './bootstrap.min.css'
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client' for React 18
+import { Provider } from 'react-redux';
+import store from './store';
+// import './index.css';
+import './bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// Use createRoot for React 18
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
